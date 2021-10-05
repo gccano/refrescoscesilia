6f4c1cb18bd951e6865d0bd2000219a75dcf15d2
gccano <91982368+gccano@users.noreply.github.com>
2021-10-05 18:14:27 +0000

    Add files via upload

---
+++ examen unidad 1 (Juana Cesilia García Cáno)/acceso.js
@@ -0,0 +1,9 @@
+function go(){
+    if (document.ingreso.usuario.value=='juana' && document.ingreso.contraseña.value=='examen')
+    {document.formingreso.submit();}
+
+    else{
+        alert("Ingrese su usuario y contraseña correctos.");
+    }
+}
+
